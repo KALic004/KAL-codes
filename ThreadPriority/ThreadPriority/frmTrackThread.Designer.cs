16d308be42ea420dad4f8355c0ef18fbb4e32113
Kalico <kimleviste2@gmail.com>
2025-09-25 20:35:42 +0800

    new

---
+++ ThreadPriority/ThreadPriority/frmTrackThread.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ThreadPriority
+{
+    partial class frmTrackThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(76, 45);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(126, 24);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "-Threat Starts-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmTrackThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 178);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "frmTrackThread";
+            this.Text = "frmTrackThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ThreadPriority/ThreadPriority/frmTrackThread.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadPriority
+{
+    public partial class frmTrackThread: Form
+    {
+        Thread threadA, threadB, threadC, threadD;
+
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            threadA = new Thread(new ThreadStart(MyThreadClass.Thread1));
+            threadB = new Thread(new ThreadStart(MyThreadClass.Thread1));
+            threadC = new Thread(new ThreadStart(MyThreadClass.Thread1));
+            threadD = new Thread(new ThreadStart(MyThreadClass.Thread1));
+
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+            threadC.Name = "Thread C";
+            threadD.Name = "Thread D";
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            threadA = new Thread(new ThreadStart(MyThreadClass.Thread2));
+            threadB = new Thread(new ThreadStart(MyThreadClass.Thread2));
+            threadC = new Thread(new ThreadStart(MyThreadClass.Thread2));
+            threadD = new Thread(new ThreadStart(MyThreadClass.Thread2));
+
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+            threadC.Name = "Thread C";
+            threadD.Name = "Thread D";
+
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            Console.WriteLine("-End of Thread-");
+            lblStatus.Text = "-End of Thread-";
+        }
+    }
+}
