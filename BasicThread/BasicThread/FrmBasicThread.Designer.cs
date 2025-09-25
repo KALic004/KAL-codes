f43dfed0fffa0504147baaa0cc81fbad0d321495
Kalico <kimleviste2@gmail.com>
2025-09-25 18:16:00 +0800

    Add files via upload

---
+++ BasicThread/BasicThread/FrmBasicThread.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BasicThread
+{
+    partial class FrmBasicThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(54, 40);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(199, 24);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "-Before starting thread-";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnRun.Location = new System.Drawing.Point(111, 97);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // FrmBasicThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 153);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "FrmBasicThread";
+            this.Text = "BasicThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ BasicThread/BasicThread/FrmBasicThread.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicThread
+{
+    public partial class FrmBasicThread: Form
+    {
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Thread threadA = new Thread(new ThreadStart(MyThreadClass.Thread1));
+            Thread threadB = new Thread(new ThreadStart(MyThreadClass.Thread1));
+
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+
+            threadA.Start();
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            Console.WriteLine("-End of Thread-");
+            lblStatus.Text = "-End of Thread-";
+        }
+    }
+}
